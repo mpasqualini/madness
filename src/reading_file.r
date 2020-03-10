88150252a040d99f71a2a46d908690ccb554414d
mpasqualini <mariana.pasqualini@sympla.com.br>
2020-03-10 18:35:33 -0300

    changes in the reading_file function

--- src/reading_file.r
+++ src/reading_file.r
@@ -3,7 +3,17 @@ library(here)
 library(readr)
 
 reading_file <- function(file_path, file_name) {
-  list.files(path = here("data/raw/2020DataFiles/2020-Womens-Data/"), pattern = "*.csv") %>% 
-    map_df(read_csv)
+  
+  list.files(path = "C:/Users/mariana.pasqualini/madness/data/raw/2020DataFiles/2020-Womens-Data/", pattern = "*.csv") %>% 
+    lapply(read_csv)
 }
 
+
+arquivos <- list.files(path = here("data/raw/2020DataFiles/2020-Womens-Data"), pattern = "*.csv", full.names=T)
+map_df(read_csv(file.path(here("data/raw/2020DataFiles/2020-Womens-Data"))), arquivos) %>% 
+  
+a <- map(arquivos, read_csv)
+
+list.files(path = here("data/raw/2020DataFiles/2020-Womens-Data"), pattern = "*.csv", full.names=T) %>% 
+  map(read_csv) %>% 
+    do.call()
