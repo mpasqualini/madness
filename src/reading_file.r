3974cd22d4cd21d63bf5cb1a72f19d8a28c85cc9
mpasqualini <mariana.pasqualini@sympla.com.br>
2020-03-10 16:59:54 -0300

    change in the src

--- src/reading_file.r
+++ src/reading_file.r
@@ -1,3 +1,9 @@
+library(purrr)
+library(here)
+library(readr)
+
 reading_file <- function(file_path, file_name) {
-  read.csv(here(file_path, file_name))
+  list.files(path = here("data/raw/2020DataFiles/2020-Womens-Data/"), pattern = "*.csv") %>% 
+    map_df(read_csv)
 }
+
